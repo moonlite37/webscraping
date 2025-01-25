41e0fb473fd25d01e86354ee5911f4eb6df317fa
Howard Li <howard.li237@gmail.com>
2025-01-25 08:10:41 -0800

    test

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-print("test")
-print("test")
+import urllib.request
+a = [5, 2, 9, 1, 5, 6]
+
+# "reverse= True" helps sorted() to arrange the element 
+#from largest to smallest elements
+res = sorted(a, reverse=True)
+print(res)
+urllib.request.urlretrieve("https://www.tiktok.com/@redditstory4641/video/7309523593170390315", 'video_name.mp4')
